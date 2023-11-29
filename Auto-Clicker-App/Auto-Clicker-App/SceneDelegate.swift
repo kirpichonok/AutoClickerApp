2d44ac627c7136316756a87f1961e70a736b96cb
kirpichonok <ivankirpichonok@gmail.com>
2023-11-29 09:19:30 +0300

    Update AppDelegate, SceneDelegate: bring the code in line with swiftlint rules.

--- Auto-Clicker-App/Auto-Clicker-App/SceneDelegate.swift
+++ Auto-Clicker-App/Auto-Clicker-App/SceneDelegate.swift
@@ -3,9 +3,7 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
-    func scene(_ scene: UIScene, willConnectTo _: UISceneSession, options _: UIScene.ConnectionOptions) {
-        guard (scene as? UIWindowScene) != nil else { return }
-    }
+    func scene(_: UIScene, willConnectTo _: UISceneSession, options _: UIScene.ConnectionOptions) {}
 
     func sceneDidDisconnect(_: UIScene) {}
 
