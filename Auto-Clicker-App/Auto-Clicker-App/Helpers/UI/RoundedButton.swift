2a10046aaf364dd1bc5f0bdcdb09b3d81fc9e65a
kirpichonok <ivankirpichonok@gmail.com>
2023-12-04 12:44:12 +0300

    Add RoundedButton file.

---
+++ Auto-Clicker-App/Auto-Clicker-App/Helpers/UI/RoundedButton.swift
@@ -0,0 +1,40 @@
+import UIKit
+
+class RoundedButton: UIButton {
+    // MARK: - Initialization
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        viewSetup()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        viewSetup()
+    }
+
+    // MARK: - Method
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        gradientLayer.frame = bounds
+    }
+
+    func setGradientBackground(with colors: [CGColor]) {
+        gradientLayer.colors = colors
+        setNeedsLayout()
+    }
+
+    // MARK: - Private property
+
+    private let gradientLayer = CAGradientLayer()
+
+    // MARK: - Private method
+
+    private func viewSetup() {
+        layer.cornerRadius = bounds.size.height / 2
+        clipsToBounds = true
+        gradientLayer.frame = bounds
+        layer.addSublayer(gradientLayer)
+    }
+}
