c603cdcb1d49d1e33ff7f1bd694166171d7fa501
kirpichonok <ivankirpichonok@gmail.com>
2023-12-06 16:28:46 +0300

    Add MainScreenVM file, update MainScreenVC, connect them to simulate touches.

--- Auto-Clicker-App/Auto-Clicker-App/MainScreen/MainScreenVC.swift
+++ Auto-Clicker-App/Auto-Clicker-App/MainScreen/MainScreenVC.swift
@@ -1,3 +1,4 @@
+import Combine
 import UIKit
 import WebKit
 
@@ -20,6 +21,40 @@ final class MainScreenVC: UIViewController {
         super.viewDidLoad()
         insertGradientLayer()
         startButton.setGradientBackground(with: Gradient.purple)
+
+        textFieldView.delegate = self
+        webView.navigationDelegate = self
+
+        bindViewModel()
+        pointView.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(handleCardTapped)))
+        startButton.addTarget(self, action: #selector(pointDidSet), for: .touchUpInside)
+    }
+
+    // MARK: - View Private Methods
+
+    private func bindViewModel() {
+        viewModel.$touches
+            .sink { [weak self] point in
+                guard let self, let point else {
+                    return
+                }
+
+                let touchPointInWebView = webView.convert(point, from: view)
+                simulateTouchAt(point: touchPointInWebView)
+            }
+            .store(in: &cancellables)
+
+        viewModel.$urlString
+            .assign(to: \.text, on: textFieldView)
+            .store(in: &cancellables)
+
+        viewModel.$url
+            .sink { [weak self] url in
+                if let self, let url {
+                    webView.load(URLRequest(url: url))
+                }
+            }
+            .store(in: &cancellables)
     }
 
     // MARK: - Private Methods
@@ -29,5 +64,54 @@ final class MainScreenVC: UIViewController {
         gradientLayer.frame = view.bounds
         gradientLayer.colors = Gradient.background
         view.layer.insertSublayer(gradientLayer, at: 0)
+    }
+
+    @objc private func handleCardTapped(_ gesture: UIPanGestureRecognizer) {
+        if let pointView = gesture.view {
+            let point = gesture.translation(in: view)
+
+            pointView.center = CGPoint(x: pointView.center.x + point.x, y: pointView.center.y + point.y)
+            gesture.setTranslation(CGPoint.zero, in: view)
+        }
+    }
+
+    @objc private func pointDidSet() {
+        viewModel.pointLocation = pointView.center
+    }
+
+    private func simulateTouchAt(point: CGPoint) {
+        webView.evaluateJavaScript("document.elementFromPoint(\(point.x), \(point.y)).click();") { _, _ in
+            print("clicked on \(point)")
+        }
+    }
+
+    // MARK: - Private Property
+
+    private var cancellables = Set<AnyCancellable>()
+    private var viewModel = MainScreenVM()
+}
+
+// MARK: - UITextFieldDelegate
+
+extension MainScreenVC: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+
+        if let urlString = textField.text,
+           !urlString.isEmpty {
+            viewModel.urlInput.send(urlString)
+        }
+
+        return true
+    }
+}
+
+// MARK: - WKNavigationDelegate
+
+extension MainScreenVC: WKNavigationDelegate {
+    // swiftlint:disable implicitly_unwrapped_optional
+    func webView(_ webView: WKWebView, didFinish _: WKNavigation!) {
+        // swiftlint:enable implicitly_unwrapped_optional
+        textFieldView.text = webView.url?.absoluteString
     }
 }
---
+++ Auto-Clicker-App/Auto-Clicker-App/MainScreen/MainScreenVM.swift
@@ -0,0 +1,69 @@
+import Combine
+import Foundation
+
+class MainScreenVM: ObservableObject {
+    /// The point to simulate touches.
+    var pointLocation: CGPoint? {
+        didSet {
+            if pointLocation != nil {
+                startTapping()
+            }
+        }
+    }
+
+    /// The string in the address line.
+    @MainActor @Published var urlString: String? {
+        didSet {
+            if let urlString,
+               let url = URL(string: urlString) {
+                self.url = url
+            }
+        }
+    }
+
+    /// The point where the touch should be simulated.
+    @MainActor @Published var touches: CGPoint?
+
+    /// URL address obtained from the user input.
+    @MainActor @Published var url: URL?
+
+    /// Handles inputted string data as a URL and broadcasts the result.
+    let urlInput = PassthroughSubject<String, Never>()
+
+    init() {
+        urlInput
+            .map { urlString in
+                if !urlString.hasPrefix("https://") && !urlString.hasPrefix("http://") {
+                    return "https://" + urlString
+                }
+
+                return urlString
+            }
+            .assign(to: \.urlString, on: self)
+            .store(in: &cancellables)
+    }
+
+    /// Emits a specified amount of touch simulations with a specified time interval.
+    private func startTapping() {
+        timer = Timer.scheduledTimer(withTimeInterval: timeInterval,
+                                     repeats: true) { [weak self] timer in
+            guard let self else {
+                return
+            }
+            guard counter > 0 else {
+                timer.invalidate()
+                return
+            }
+
+            counter -= 1
+            DispatchQueue.main.async {
+                self.touches = self.pointLocation
+            }
+        }
+    }
+
+    private var counter = 3
+    private var timeInterval = 2.0
+    private var timer: Timer?
+    private var cancellables = Set<AnyCancellable>()
+}
