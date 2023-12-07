de1a1d9b8d522e2abb997296512a2882f6339550
kirpichonok <ivankirpichonok@gmail.com>
2023-12-07 18:29:23 +0300

    Update MainScreenView: refactor code, change functions names, add methods for back, forward navigation.

--- Auto-Clicker-App/Auto-Clicker-App/MainScreen/View/MainScreenView.swift
+++ Auto-Clicker-App/Auto-Clicker-App/MainScreen/View/MainScreenView.swift
@@ -6,7 +6,7 @@ final class MainScreenView: UIViewController {
     // MARK: - Properties
 
     @IBOutlet private var startButton: RoundedButton!
-    @IBOutlet private var backButton: UIButton!
+    @IBOutlet private var goBackButton: UIButton!
     @IBOutlet private var goForwardButton: UIButton!
     @IBOutlet private var settingsButton: UIButton!
     @IBOutlet private var refreshButton: UIButton!
@@ -19,18 +19,52 @@ final class MainScreenView: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        insertGradientLayer()
-        startButton.setGradientBackground(with: Gradient.purple)
-
         textFieldView.delegate = self
         webView.navigationDelegate = self
 
+        viewSetup()
         bindViewModel()
-        pointView.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(handleCardTapped)))
-        startButton.addTarget(self, action: #selector(pointDidSet), for: .touchUpInside)
+        buttonsActionSetup()
+        addSubscribers()
     }
 
-    // MARK: - View Private Methods
+    // MARK: - Actions
+
+    @objc private func handlePanPoint(_ gesture: UIPanGestureRecognizer) {
+        guard let pointView = gesture.view else {
+            return
+        }
+        let initialCenter = pointView.center
+        let translation = gesture.translation(in: view)
+
+        if gesture.state != .cancelled {
+            pointView.center = CGPoint(x: initialCenter.x + translation.x, y: initialCenter.y + translation.y)
+            gesture.setTranslation(CGPoint.zero, in: view)
+        } else {
+            pointView.center = initialCenter
+        }
+    }
+
+    @objc private func pointDidSet() {
+        viewModel.pointLocation = pointView.center
+    }
+
+    @objc private func goBack() {
+        webView.goBack()
+    }
+
+    @objc private func goForward() {
+        webView.goForward()
+    }
+
+    @objc private func reloadPage() {
+        webView.reload()
+    }
+
+    // MARK: - Private properties
+
+    private var cancellables = Set<AnyCancellable>()
+    private var viewModel = MainScreenVM()
 
     private func bindViewModel() {
         viewModel.$touches
@@ -59,24 +93,53 @@ final class MainScreenView: UIViewController {
 
     // MARK: - Private Methods
 
-    private func insertGradientLayer() {
+    private func viewSetup() {
+        insertGradientScreenBackground()
+        startButton.setGradientBackground(with: Gradient.purple)
+        pointView.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(handlePanPoint)))
+        webView.allowsBackForwardNavigationGestures = true
+    }
+
+    private func buttonsActionSetup() {
+        startButton.addTarget(self, action: #selector(pointDidSet), for: .touchUpInside)
+        goBackButton.addTarget(self, action: #selector(goBack), for: .touchUpInside)
+        goForwardButton.addTarget(self, action: #selector(goForward), for: .touchUpInside)
+        refreshButton.addTarget(self, action: #selector(reloadPage), for: .touchUpInside)
+    }
+
+    private func insertGradientScreenBackground() {
         let gradientLayer = CAGradientLayer()
         gradientLayer.frame = view.bounds
         gradientLayer.colors = Gradient.background
         view.layer.insertSublayer(gradientLayer, at: 0)
     }
 
-    @objc private func handleCardTapped(_ gesture: UIPanGestureRecognizer) {
-        if let pointView = gesture.view {
-            let point = gesture.translation(in: view)
+    private func addSubscribers() {
+        webView.publisher(for: \.canGoBack)
+            .sink { [weak self] canGoBack in
+                guard let self else {
+                    return
+                }
+                if canGoBack {
+                    self.goBackButton.tintColor = .accent
+                } else {
+                    self.goBackButton.tintColor = .systemGray
+                }
+            }
+            .store(in: &cancellables)
 
-            pointView.center = CGPoint(x: pointView.center.x + point.x, y: pointView.center.y + point.y)
-            gesture.setTranslation(CGPoint.zero, in: view)
-        }
-    }
-
-    @objc private func pointDidSet() {
-        viewModel.pointLocation = pointView.center
+        webView.publisher(for: \.canGoForward)
+            .sink { [weak self] canGoForward in
+                guard let self else {
+                    return
+                }
+                if canGoForward {
+                    self.goForwardButton.tintColor = .accent
+                } else {
+                    self.goForwardButton.tintColor = .systemGray
+                }
+            }
+            .store(in: &cancellables)
     }
 
     private func simulateTouchAt(point: CGPoint) {
@@ -84,11 +147,6 @@ final class MainScreenView: UIViewController {
             print("clicked on \(point)")
         }
     }
-
-    // MARK: - Private Property
-
-    private var cancellables = Set<AnyCancellable>()
-    private var viewModel = MainScreenVM()
 }
 
 // MARK: - UITextFieldDelegate
