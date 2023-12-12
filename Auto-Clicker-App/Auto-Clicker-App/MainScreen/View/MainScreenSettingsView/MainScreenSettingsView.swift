a7accca03cb986e9bb48ae24e6608b316a7ff986
kirpichonok <ivankirpichonok@gmail.com>
2023-12-12 10:29:56 +0300

    Update MainScreenSettingsView, Main storyboard: add outlets, set GradientBackgroundView.

--- Auto-Clicker-App/Auto-Clicker-App/MainScreen/View/MainScreenSettingsView/MainScreenSettingsView.swift
+++ Auto-Clicker-App/Auto-Clicker-App/MainScreen/View/MainScreenSettingsView/MainScreenSettingsView.swift
@@ -1,3 +1,29 @@
 import UIKit
 
-class MainScreenSettingsView: UIViewController {}
+class MainScreenSettingsView: UIViewController {
+    // MARK: - IBOutlets
+
+    @IBOutlet private var numberOfPointersLabel: UILabel!
+    @IBOutlet private var numberOfClicksLabel: UILabel!
+    @IBOutlet private var intervalTimeLabel: UILabel!
+
+    @IBOutlet private var numberOfPointersSlider: UISlider!
+    @IBOutlet private var numberOfClicksSlider: UISlider!
+    @IBOutlet private var intervalTimeSlider: UISlider!
+
+    @IBOutlet private var startButton: UIButton!
+
+    // MARK: - View Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        viewSetup()
+    }
+
+    // MARK: - Actions
+
+    private func viewSetup() {
+        (view.layer as? CAGradientLayer)?.colors = Gradient.background
+        startButton.configuration = .capsuleWithBackground(gradient: Gradient.purple)
+    }
+}
