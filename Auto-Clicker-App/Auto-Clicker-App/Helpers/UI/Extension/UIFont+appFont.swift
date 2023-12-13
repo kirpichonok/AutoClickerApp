0b8ecffdbb196d0af9d00d36da00ecebb228fafb
kirpichonok <ivankirpichonok@gmail.com>
2023-12-13 15:13:02 +0300

    Update UIFont+appFont: add system font and assertion.

--- Auto-Clicker-App/Auto-Clicker-App/Helpers/UI/Extension/UIFont+appFont.swift
+++ Auto-Clicker-App/Auto-Clicker-App/Helpers/UI/Extension/UIFont+appFont.swift
@@ -2,10 +2,22 @@ import UIKit
 
 extension UIFont {
     static func appSemibold(size: CGFloat) -> UIFont? {
-        UIFont(name: "SFPro-Semibold", size: size)
+        if let font = UIFont(name: "SFPro-Semibold", size: size) {
+            return font
+        }
+        else {
+            assertionFailure()
+            return UIFont.systemFont(ofSize: size, weight: .regular)
+        }
     }
 
-    static func appRegular(size: CGFloat) -> UIFont? {
-        UIFont(name: "SFPro-Regular", size: size)
+    static func appRegular(size: CGFloat) -> UIFont {
+        if let font = UIFont(name: "SFPro-Regular", size: size) {
+            return font
+        }
+        else {
+            assertionFailure()
+            return UIFont.systemFont(ofSize: size, weight: .regular)
+        }
     }
 }
